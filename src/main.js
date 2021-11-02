a4b6ce152e47dfd5d294cbb8cc3c17abedc6d829
ben <tbeben77@gmail.com>
2021-11-02 22:25:29 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -3,11 +3,25 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+const firebaseConfig = {
+  apiKey: "AIzaSyBSwvgCz55eJNilSBTzB9eux4V_Tt3LU9k",
+  authDomain: "test-8a676.firebaseapp.com",
+  projectId: "test-8a676",
+  storageBucket: "test-8a676.appspot.com",
+  messagingSenderId: "696690019746",
+  appId: "1:696690019746:web:cdf456547af002ca3376fb",
+  measurementId: "G-LLLPN2GSX8",
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
